52319d417e5dd8cc846bc94a65c6d184b23409a5
crazysolutions25-tech <crazysolutions25@gmail.com>
2026-01-02 10:27:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,16 @@ if (newsContainer) {
 
   loadNews();
 }
+
+/* ACCORDION */
+document.querySelectorAll(".accordion-header").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const content = btn.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
